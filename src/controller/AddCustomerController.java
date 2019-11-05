409b9a965807481ffe4c0c99880837ac368097e8
DilushaKumari <dilushakumaridt@gmail.com>
2019-11-05 21:36:43 +0530

    Initial Commit

--- src/controller/AddCustomerController.java
+++ src/controller/AddCustomerController.java
@@ -2,10 +2,15 @@ package controller;
 
 import DB.DBConnection;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -15,7 +20,7 @@ public class AddCustomerController {
     public TextField txtName;
     public TextField txtAddress;
     public Button btnSave;
-
+    public Button btnHome;
 
 
     public void btnSave_OnAction(ActionEvent actionEvent) {
@@ -29,6 +34,9 @@ public class AddCustomerController {
             int affectedRow = insertQuery.executeUpdate();
             if(affectedRow>0){
                 new Alert(Alert.AlertType.INFORMATION,"New Customer Added Successfully...").show();
+                txtAddress.clear();
+                txtId.clear();
+                txtName.clear();
             }
 
         } catch (SQLException e) {
@@ -36,5 +44,13 @@ public class AddCustomerController {
         }
 
 
+    }
+
+    public void btnHome_OnAction(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(this.getClass().getResource("/view/DashBoardForm.fxml"));
+        Scene subScene = new Scene(root);
+        Stage primaryStage = (Stage) this.btnSave.getScene().getWindow();
+        primaryStage.setScene(subScene);
+        primaryStage.centerOnScreen();
     }
 }
